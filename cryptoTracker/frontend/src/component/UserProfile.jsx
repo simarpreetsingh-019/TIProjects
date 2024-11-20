840aae111401cd9d4e445327faff92d4e97ee403
deep0304 <lakshyadeeo0304@gmail.com>
2024-11-20 18:07:50 +0530

    Changing Images Srcs

--- cryptoTracker/frontend/src/component/UserProfile.jsx
+++ cryptoTracker/frontend/src/component/UserProfile.jsx
@@ -1,25 +1,19 @@
-// import React from 'react';
-
+import React from "react";
+import image3 from "../assets/images/image3.png";
 import { useState } from "react";
 
-function UserProfile ({onLogout})
-{
-
+function UserProfile({ onLogout }) {
   return (
-    <div className="flex justify-between whitespace-nowrap max-md:mt-10"  >
-    <div className="flex gap-[10px] items-center" >
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/5c5b5033678c83173f3e2e5685372332ea71c484967e9438720ecc1ba656a6ca?placeholderIfAbsent=true&apiKey=9825bd02a58246fc9d930bec7baef96a"
-        className="object-contain shrink-0 w-12 aspect-square"
-        alt="user-icon"
-
-      />
+    <div className="flex justify-between whitespace-nowrap max-md:mt-10">
+      <div className="flex gap-[10px] items-center">
+        <img
+          loading="lazy"
+          src={image3}
+          className="object-contain shrink-0 w-12 aspect-square"
+          alt="user-icon"
+        />
+      </div>
     </div>
-
-
-  </div>
-
   );
 }
 
