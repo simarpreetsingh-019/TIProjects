5a9932dceed8edd4bd756d8aa191511c5de921d2
KR$NA <78905769+KrishnaTiwariCoder@users.noreply.github.com>
2025-01-20 22:34:08 +0530

    some fixes

--- TEZ-TUNES/client/src/helpers/upload.js
+++ TEZ-TUNES/client/src/helpers/upload.js
@@ -2,15 +2,13 @@
 import { PinataSDK } from "pinata-web3";
 
 export const pinata = new PinataSDK({
-  pinataJwt:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI1YTg1NzEyYi1mYjQxLTQ1NzktYjViMi1jOTNlZGExZDU0MmYiLCJlbWFpbCI6ImtyaXNobmF0aXdhcmlkdGVhQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6IkZSQTEifSx7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6Ik5ZQzEifV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiJjYmY1YTRkYjIxOWM4YjY1YTE5YyIsInNjb3BlZEtleVNlY3JldCI6ImNlMWFhODM3ZTBkNTMwMTI2ZGZlZTlmZTc2ZDYxOTY4ZjNmNDEwNmM0MjBkYjBkZTUzNGM5ZTBhMjhjOTIwZWYiLCJleHAiOjE3NjE4MzEzOTV9.QUCIcs2aJ7_F8fLbuaUrQy4XTyToC7ZzSwz0t30dO0I",
-  pinataGateway: "salmon-general-eel-568.mypinata.cloud",
+  pinataJwt: "your_api_key_here",
+  pinataGateway: "https://gateway.pinata.cloud/ipfs",
 });
 
 export async function uploadToPinata(file) {
-  const apiKey = "cbf5a4db219c8b65a19c";
-  const secretApiKey =
-    "ce1aa837e0d530126dfee9fe76d61968f3f4106c420db0d e534c9e0a28c920ef";
+  const apiKey = "your_api_key_here";
+  const secretApiKey = "your_secret_api_key_here";
   const formData = new FormData();
   formData.append("file", file);
 
